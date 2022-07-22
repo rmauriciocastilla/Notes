5598345127c470fe39d36f32f51050a92282a023
rmauriciocastilla <rmauriciocastilla@gmail.com>
2022-07-22 16:31:48 -0500

    fixing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route,Switch} from 'react-router-dom';
+import {Route,Switch, Redirect} from 'react-router-dom';
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
