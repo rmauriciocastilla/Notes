64244730f63aa04836960eccde085de6cc489327
rmauriciocastilla <rmauriciocastilla@gmail.com>
2022-07-22 16:08:23 -0500

    updating

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,14 +3,14 @@ export const GET_ALL_NOTES = "GET_ALL_NOTES";
 export const GET_NOTE = "GET_NOTE";
 
 export const getAllNotes = () => (dispatch) =>{
-    return axios.get('http://localhost:3001/note')
+    return axios.get('https://rmauriciocastilla-notes-node.herokuapp.com/')
             .then(response=>response.data)
             .then(notes => dispatch({type: GET_ALL_NOTES, payload: notes}))
             .catch(e=>console.log(e))
 }
 
 export const deleteNote = (id) => () =>{
-    return axios.delete(`http://localhost:3001/note/delete/${id}`)
+    return axios.delete(`https://rmauriciocastilla-notes-node.herokuapp.com/${id}`)
             .then(response=>response.data)
             .then(msg=>console.log(msg))
             .catch(e=>console.log(e));
@@ -18,7 +18,7 @@ export const deleteNote = (id) => () =>{
 }
 
 export const updateNote = (id, payload) => () =>{
-    return axios.put(`http://localhost:3001/note/update/${id}`, payload)
+    return axios.put(`https://rmauriciocastilla-notes-node.herokuapp.com/${id}`, payload)
             .then(response=>response.data)
             .then(e => alert(e))
             .catch(e=>console.log(e));
@@ -26,14 +26,14 @@ export const updateNote = (id, payload) => () =>{
 }
 
 export const createNote = (payload) => () =>{
-    return axios.post('http://localhost:3001/note', payload)
+    return axios.post('https://rmauriciocastilla-notes-node.herokuapp.com/', payload)
             .then(response=>response.data)
             .then(e=>alert(e))
             .catch(e=>console.log(e));
 }
 
 export const getNote = (id) => async (dispatch) =>{
-    return await axios.get(`http://localhost:3001/note/mynote/${id}`)
+    return await axios.get(`https://rmauriciocastilla-notes-node.herokuapp.com/${id}`)
             .then(response=>response.data)
             .then(note => dispatch({type: GET_NOTE, payload: note}))
             .catch(e=>console.log(e))
